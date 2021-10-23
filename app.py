5e4c34210fc44d46b2929755c63d4f062f952733
Dark-Senpai <87824804+Dark-Senpai@users.noreply.github.com>
2021-10-23 17:18:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 # @dark-senpai
 
-#_______________ Import Vital items________________#
+#_______________ Import Vital items ________________#
 
 from telethon import events
 from config import bot, bot_username
-from TelethonDownloader import fast_upload, fast_download
+from TelethonDownloader import progress_upload, progress_download
 import subprocess
 import asyncio
 import os
+
+#_______________ Events _________________#
+
+
 
