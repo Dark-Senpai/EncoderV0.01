bb3d26e1681b0f0da2ffa152608d7b90261f0cc0
Dark-Senpai <87824804+Dark-Senpai@users.noreply.github.com>
2021-10-23 20:35:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,18 @@
-# @dark-senpai 
+#      This file is part of the EncoderV0.01 distribution.
+#    Copyright (c) 2021 Dark-Senpai
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3.
+#
+#    This program is distributed in the hope that it will be useful, but
+#    WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#    General Public License for more details.
+#
+# License can be found in <
+# https://github.com/Dark-Senpai/EncoderV0.01/blob/main/License> .
+
 import os
 from telethon import TelegramClient
 
