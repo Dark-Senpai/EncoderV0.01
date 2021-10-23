47ac72de2bf6de77b383da65a9ee8e3b940ce363
Dark-Senpai <87824804+Dark-Senpai@users.noreply.github.com>
2021-10-23 21:47:03 +0530

    Update utlis.py
    Added the license everywhere!

--- utlis.py
+++ utlis.py
@@ -1,3 +1,19 @@
+#      This file is part of the EncoderV0.01 distribution.
+#    Copyright (c) 2021 Dark-Senpai
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3.
+#
+#    This program is distributed in the hope that it will be useful, but
+#    WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#    General Public License for more details.
+#
+# License can be found in <
+# https://github.com/Dark-Senpai/EncoderV0.01/blob/main/License> .
+
+
 import asyncio
 import glob
 import math
