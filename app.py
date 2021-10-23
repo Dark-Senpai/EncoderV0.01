21c2a25a302d477a7ba652abdf8faeb485f8f925
Dark-Senpai <87824804+Dark-Senpai@users.noreply.github.com>
2021-10-23 17:22:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 
 from telethon import events
 from config import bot, bot_username
-from TelethonDownloader import progress_upload, progress_download
+from TelethonDownloader.TelethonLibrary.fast_download import progress_upload, progress_download
 import subprocess
 import asyncio
 import os
