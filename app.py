97d475b9d8dedc2326431ca1c7558e6ab50a8eff
HaloDragon7410 <68484158+HaloDragon7410@users.noreply.github.com>
2021-10-24 15:19:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ async def encode(event):
   file = file.split("/")[-1]
   print(file)
   await fuk.edit("⏰ Encoding In **Progress**")
-  await bash(f'ffmpeg -i "{file}" -map 0 -c:v libx265 -crf 30 -c:a libopus -ab 40k  -s 854x480 -pix_fmt yuv420p -preset veryfast "[ENCODED] {file}"')
+  await bash(f'ffmpeg -i "./downloads/{file}" -map 0 -c:v libx265 -crf 30 -c:a libopus -ab 40k  -s 854x480 -pix_fmt yuv420p -preset veryfast "./downloads/[ENCODED] {file}"')
   final_file = await fast_upload(bot, f"./downloads/[ENCODED] {file}", fuk)
   os.remove(f"./downloads/{file}")
   os.remove(f"./downloads/[ENCODED] {file}")
