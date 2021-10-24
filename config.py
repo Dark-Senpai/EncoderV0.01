6917ce04c07b82cea347258a4e03d2b8c1b74ebe
Dark-Senpai <87824804+Dark-Senpai@users.noreply.github.com>
2021-10-24 12:01:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@
 # https://github.com/Dark-Senpai/EncoderV0.01/blob/main/License> .
 
 from telethon import TelegramClient
-from decouple import config
+import os
 import logging
 import time
 from telethon import TelegramClient
@@ -23,13 +23,18 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # Basics
-APP_ID = config("APP_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-BOT_USERNAME = config("BOT_USERNAME", default=None)
+API_ID = os.environ.get('API_ID')
+API_HASH = os.environ.get('API_HASH')
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+BOT_USERNAME = os.environ.get('BOT_USERNAME')
 
-bot = TelegramClient('bot', APP_ID, API_HASH).start(bot_token=BOT_TOKEN)
-APP_ID = 3281305
+#APP_ID = config("APP_ID", default=None, cast=int)
+#API_HASH = config("API_HASH", default=None)
+#BOT_TOKEN = config("BOT_TOKEN", default=None)
+#BOT_USERNAME = config("BOT_USERNAME", default=None)
+
+bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+API_ID = 3281305
 API_HASH = "a9e62ec83fe3c22379e3e19195c8b3f6" 
 BOT_TOKEN = "2011046095:AAEBBAkwF9UVvxgPqPWh5bdlWaJEU_OhnFk" 
 BOT_USERNAME = "telethonBot_bot"
