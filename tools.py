bba3c57a249012d42d0b4372466135f7a2dd25f9
Dark-Senpai <87824804+Dark-Senpai@users.noreply.github.com>
2021-10-24 22:01:19 +0530

    Update tools.py

--- tools.py
+++ tools.py
@@ -22,14 +22,17 @@ import re
 import sys
 import time
 import pathlib 
-
+from traceback import format_exc 
 from datetime import datetime as dt
-from FastTelethon import upload_file, download_file
+from FastTelethon import upload_file as uploadable 
+from FastTelethon import download_file as downloadable 
 
 # --------------------------------------------------------------------- #
 
 
-# --------------------------------------------------------------------- #
+# ---------------------------------------------------------------------
+
+
 async def bash(cmd):
     process = await asyncio.create_subprocess_shell(
         cmd,
@@ -41,9 +44,9 @@ async def bash(cmd):
     out = stdout.decode().strip()
     return out, err
 
-#_____________________ Fast Download / Fast Upload ____________________#
 
-async def progress_upload(file, name, taime, event, msg):
+#~~~~~~~~~~~~~~~~ Fast Dl/Ul ~~~~~~~~~~~~~#
+async def uploader(file, name, taime, event, msg):
     with open(file, "rb") as f:
         result = await uploadable(
             client=event.client,
@@ -62,7 +65,7 @@ async def progress_upload(file, name, taime, event, msg):
     return result
 
 
-async def progress_download(filename, file, event, taime, msg):
+async def downloader(filename, file, event, taime, msg):
     with open(filename, "wb") as fk:
         result = await downloadable(
             client=event.client,
@@ -79,7 +82,62 @@ async def progress_download(filename, file, event, taime, msg):
             ),
         )
     return result
-#______________________ Progress/Status Bar ______________________#
+
+
+
+# --------------------------Media Funcs-------------------------------- #
+
+
+def make_html_telegraph(title, author, text):
+    client = TelegraphPoster(use_api=True)
+    client.create_api_token(title)
+    page = client.post(
+        title=title,
+        author=author,
+        author_url="https://t.me/TeamUltroid",
+        text=text,
+    )
+    return page["url"]
+
+
+def mediainfo(media):
+    xx = str((str(media)).split("(", maxsplit=1)[0])
+    m = ""
+    if xx == "MessageMediaDocument":
+        mim = media.document.mime_type
+        if mim == "application/x-tgsticker":
+            m = "sticker animated"
+        elif "image" in mim:
+            if mim == "image/webp":
+                m = "sticker"
+            elif mim == "image/gif":
+                m = "gif as doc"
+            else:
+                m = "pic as doc"
+        elif "video" in mim:
+            if "DocumentAttributeAnimated" in str(media):
+                m = "gif"
+            elif "DocumentAttributeVideo" in str(media):
+                i = str(media.document.attributes[0])
+                if "supports_streaming=True" in i:
+                    m = "video"
+                m = "video as doc"
+            else:
+                m = "video"
+        elif "audio" in mim:
+            m = "audio"
+        else:
+            m = "document"
+    elif xx == "MessageMediaPhoto":
+        m = "pic"
+    elif xx == "MessageMediaWebPage":
+        m = "web"
+    return m
+
+
+# ------------------Some Small Funcs----------------
+
+
 def time_formatter(milliseconds):
     minutes, seconds = divmod(int(milliseconds / 1000), 60)
     hours, minutes = divmod(minutes, 60)
@@ -126,14 +184,14 @@ async def progress(current, total, event, start, type_of_ps, file_name=None):
         speed = current / diff
         time_to_completion = round((total - current) / speed) * 1000
         progress_str = "`[{0}{1}] {2}%`\n\n".format(
-            "".join("■" for i in range(math.floor(percentage / 5))),
+            "".join("●" for i in range(math.floor(percentage / 5))),
             "".join("" for i in range(20 - math.floor(percentage / 5))),
             round(percentage, 2),
         )
 
         tmp = (
             progress_str
-            + "`{0} of {1}`\n\n`• 🌠 Speed: {2}/s`\n\n`• ⏰ ETA: {3}`\n\n".format(
+            + "`{0} of {1}`\n\n`✦ Speed: {2}/s`\n\n`✦ ETA: {3}`\n\n".format(
                 humanbytes(current),
                 humanbytes(total),
                 humanbytes(speed),
@@ -142,7 +200,9 @@ async def progress(current, total, event, start, type_of_ps, file_name=None):
         )
         if file_name:
             await event.edit(
-                "`• {}`\n\n`File Name: {}`\n\n{}".format(type_of_ps, file_name, tmp)
+                "`✦ {}`\n\n`File Name: {}`\n\n{}".format(type_of_ps, file_name, tmp)
             )
         else:
-            await event.edit("`• {}`\n\n{}".format(type_of_ps, tmp))
+            await event.edit("`✦ {}`\n\n{}".format(type_of_ps, tmp))
+
+
