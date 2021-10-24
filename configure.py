173603ad1107a95dc39ba549a34b1bf95c6c332f
Dark-Senpai <87824804+Dark-Senpai@users.noreply.github.com>
2021-10-24 12:13:48 +0530

    Update configure.py

--- configure.py
+++ configure.py
@@ -1,2 +1,13 @@
 from val_cfg import get_config
-class Config(object):
+
+#______________ Vars ________________#
+class vars(object):
+  # add api_id in __init.py ( get from telegram.org )
+  API_ID = get_config('API_ID')
+  # add api_hash in __init.py ( get from telegram.org)
+  API_HASH = get_config('API_HASH')
+  # array! add bot_token recieved from @botfather from telegram 
+  BOT_TOKEN = get_config('BOT_TOKEN')
+  # oof ! add your bot username !
+  BOT_USERNAME = get_config('BOT_USERNAME')
+#_______________ Done ___________#
