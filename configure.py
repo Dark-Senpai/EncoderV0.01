f31339b119ab8efe3bcf15dcac3db1c766fb288b
Dark-Senpai <87824804+Dark-Senpai@users.noreply.github.com>
2021-10-24 12:45:05 +0530

    Update configure.py

--- configure.py
+++ configure.py
@@ -2,13 +2,19 @@ from val_cfg import get_config
 
 #______________ Vars ________________#
 class vars(object):
-  # add api_id in config.py ( get from telegram.org )
-  API_ID = get_config('API_ID')
-  # add api_hash in config.py ( get from telegram.org)
-  API_HASH = get_config('API_HASH')
-  # array! add bot_token recieved from @botfather from telegram 
-  BOT_TOKEN = get_config('BOT_TOKEN')
-  # oof ! add your bot username !
-  BOT_USERNAME = get_config('BOT_USERNAME')
+  try:
+    # add api_id in config.py ( get from telegram.org )
+    API_ID = get_config('API_ID')
+    # add api_hash in config.py ( get from telegram.org)
+    API_HASH = get_config('API_HASH')
+    # array! add bot_token recieved from @botfather from telegram 
+    BOT_TOKEN = get_config('BOT_TOKEN')
+    # oof ! add your bot username !
+    BOT_USERNAME = get_config('BOT_USERNAME')
+  except Exception as e:
+    print(str(e))
+    exit()
 
+    
+  
 #_______________ Done ___________#
